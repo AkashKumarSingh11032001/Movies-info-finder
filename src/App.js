53e842cf217766c8f4d7b67b188b35b154be34b0
Akash Kumar Singh <akashkumarsingh11032001@gmail.com>
2022-04-05 14:05:00 +0530

    App css and svg icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
+
+const ApiKey = "http://www.omdbapi.com/?i=tt3896198&apikey=99f9c1f9"
 
 const App = () => {
+
+    useEffect(() => {
+        searchMovies("IronMan");
+    })
+
+    const searchMovies = async (title) => {
+        const response =  await fetch(`${ApiKey}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
 
     return (
 
         <div>
             <h1>Hello!</h1>
+            
         </div>
 
     );
